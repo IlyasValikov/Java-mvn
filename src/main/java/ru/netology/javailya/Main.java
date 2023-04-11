85592ba8ab7e6d8c52153f3e0684eda232a70278
Ilya Valikov <valikov.skeleton1977@yandex.ru>
2023-04-11 22:47:15 +0300

    add javaqamavn

---
+++ src/main/java/ru/netology/javailya/Main.java
@@ -0,0 +1,27 @@
+package ru.netology.javailya;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        System.out.println("1. " + expected + " == ? == " + actual);
+
+        amount = 1_000_000;
+        registered = true;
+        expected = 500;
+        actual = service.calculate(amount, registered);
+
+        System.out.println("2. " + expected + " == ? == " + actual);
+
+
+    }
+}
